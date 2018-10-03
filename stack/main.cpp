ed07ceade29ea1b42547cca0a877b7be7f47d257
Arv1k <viktorov.ak@phystech.edu>
2018-10-04 01:11:21 +0300

    New project

---
+++ stack/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cassert>
+
+typedef double        data_t;
+typedef unsigned long size_t;
+
+struct Stack {
+    data_t* Data;
+    size_t  Size;
+    size_t  Capacity;
+};
+
+int main() {
+    
+}
